dea942057fb16830445a3f78ce9953383fee19c6
0xd34df00d <0xd34df00d@gmail.com>
2018-06-11 00:45:47 -0400

    Stub implementation of sequential interpreter.

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,10 @@ module Main where
 
 import Prelude hiding(read)
 
+import qualified Data.Vector.Unboxed as VU
+
 import Control.Monad.Loops
+import Control.Monad.State hiding(lift)
 
 import Data.Free.Program
 
@@ -31,6 +34,34 @@ add p a = snd <$> do
     value <- read p
     success <- cas p value (value + a)
     pure (success, value + a)
+
+data SeqState = SeqState
+  { cells :: VU.Vector Int
+  } deriving (Eq, Ord, Show)
+
+readSeqSt :: IntPtr -> SeqState -> Int
+readSeqSt ptr st = cells st VU.! cell ptr
+
+writeSeqSt :: IntPtr -> Int -> SeqState -> SeqState
+writeSeqSt ptr val st = st { cells = cells st VU.// [(cell ptr, val)] }
+
+casSeqSt :: IntPtr -> Int -> Int -> SeqState -> (Bool, SeqState)
+casSeqSt ptr cmp val st | cmp == curVal = (True, writeSeqSt ptr val st)
+                        | otherwise = (False, st)
+  where curVal = readSeqSt ptr st
+
+runProgramSeq :: Program Op a -> State SeqState a
+runProgramSeq (Pure x) = pure x
+runProgramSeq (Bind (Read ptr) act) = do
+  val <- gets $ readSeqSt ptr
+  runProgramSeq $ act val
+runProgramSeq (Bind (Write ptr val) act) = do
+  modify' $ writeSeqSt ptr val
+  runProgramSeq $ act ()
+runProgramSeq (Bind (CAS ptr cmp val) act) = do
+  (res, st') <- gets $ casSeqSt ptr cmp val
+  put st'
+  runProgramSeq $ act res
 
 main :: IO ()
 main = undefined
